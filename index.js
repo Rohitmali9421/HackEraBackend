b56a1d0bebb3aacf2ecb930895ddd6014670b70c
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-30 16:24:05 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express")
 require('dotenv').config()
+const cors = require('cors')
 const UserRoutes=require("./Routes/User")
 const connectMongoDB = require("./config/connection")
 
@@ -7,6 +8,7 @@ const app=express()
 const URI=process.env.MONGODB_URI
 const port= process.env.PORT || 8000 
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
